b70e0861fd09b5ca8c31e8e8454202e98983e30c
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2024-07-21 22:34:03 -0300

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="text-center items-center">
       <h2 className="text-zinc-500/80 mt-9">B L O G</h2>
       <h1 className="lg:text-6xl max-sm:text-3xl font-bold lg:mb-24 bg-clip-text text-transparent bg-gradient-to-r from-zinc-50 to-blue-500">
-        Hello, I'm Luiz!
+        Hello, I&apos;m Luiz!
       </h1>
       <div className="grid lg:grid-cols-3 sm:grid-cols-1">
         {mainArticles.map((article, index) => {
